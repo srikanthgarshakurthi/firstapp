e53ba312ef33ac327c907f9c6db847e59c943624
srikanth <93856726+srikanthgarshakurthi@users.noreply.github.com>
2023-10-28 06:42:48 +0530

    init

---
+++ src/component/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <h1>Hii from React </h1>
+      <h2>My code</h2>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
